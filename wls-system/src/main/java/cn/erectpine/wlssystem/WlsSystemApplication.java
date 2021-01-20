b4e19d84ca7c717aa549cb0f5fb6a4020097e2dd
wls <1626961806@qq.com>
2021-01-20 14:36:11 +0800

    first commit

---
+++ wls-system/src/main/java/cn/erectpine/wlssystem/WlsSystemApplication.java
@@ -0,0 +1,19 @@
+package cn.erectpine.wlssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * wls系统应用
+ *
+ * @author wls
+ * @date 2021/01/20 14:28:02
+ */
+@SpringBootApplication
+public class WlsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WlsSystemApplication.class, args);
+	}
+
+}
