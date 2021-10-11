0cded8ce4b1dd6420898cfaa2f68246a2175905e
wls <1626961806@qq.com>
2021-10-11 14:18:10 +0800

    first commit

--- wls-api/wls-api-dict/src/main/java/cn/erectpine/dict/api/DictDataApi.java
+++ wls-api/wls-api-dict/src/main/java/cn/erectpine/dict/api/DictDataApi.java
@@ -3,9 +3,13 @@ package cn.erectpine.dict.api;
 import cn.erectpine.common.core.constant.ServiceNameConstants;
 import cn.erectpine.common.web.pojo.Result;
 import cn.erectpine.dict.entity.DictData;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 字典数据API
@@ -20,47 +24,46 @@ public interface DictDataApi {
     /**
      * 字典数据-分页列表
      *
-     * @param page     页面
      * @param dictData dict类型数据
-     * @return {@link Result}
+     * @return {@link Result}<{@link IPage}<{@link DictData}>>
      */
-    @PostMapping("/page")
-    Result pageDictData(DictData dictData);
+    @GetMapping("/page")
+    Result<IPage<DictData>> pageDictData(DictData dictData);
     
     /**
      * 根据id获取字典数据详情
      *
      * @param id id
-     * @return {@link Result}
+     * @return {@link Result}<{@link DictData}>
      */
     @GetMapping("/{id}")
-    Result getDictDataById(@PathVariable("id") Long id);
+    Result<DictData> getDictDataById(@PathVariable Long id);
     
     /**
      * 新增-字典数据
      *
      * @param dictData dict类型数据
-     * @return {@link Result}
+     * @return {@link Result}<{@link ?}>
      */
     @PostMapping
-    Result insertDictData(@RequestBody DictData dictData);
+    Result<?> insertDictData(@RequestBody @Validated DictData dictData);
     
     /**
      * 修改-字典数据
      *
      * @param dictData dict类型数据
-     * @return {@link Result}
+     * @return {@link Result}<{@link ?}>
      */
     @PutMapping
-    Result updateDictData(@RequestBody DictData dictData);
+    Result<?> updateDictData(@RequestBody @Validated DictData dictData);
     
     /**
      * 删除-字典数据
      *
-     * @param id id
-     * @return {@link Result}
+     * @param ids id
+     * @return {@link Result}<{@link ?}>
      */
-    @DeleteMapping("/{id}")
-    Result deleteDictData(@PathVariable("id") Long id);
+    @DeleteMapping("/{ids}")
+    Result<?> deleteDictData(@PathVariable("ids") List<Long> ids);
     
 }
