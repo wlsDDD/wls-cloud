d584a2c32a054742e6be60d605aa055b521368bb
wls <1626961806@qq.com>
2022-03-10 14:13:31 +0800

    first commit

--- wls-system/src/main/java/cn/erectpine/system/WlsSystemApplication.java
+++ wls-system/src/main/java/cn/erectpine/system/WlsSystemApplication.java
@@ -1,5 +1,6 @@
 package cn.erectpine.system;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,12 +10,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author wls
  * @since 2021/01/20 14:28:02
  */
+@Slf4j
 @SpringBootApplication
 public class WlsSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WlsSystemApplication.class, args);
-		System.err.println("(♥◠‿◠)ﾉﾞ  wls系统服务-启动完成   ლ(´ڡ`ლ)ﾞ  ");
+		log.info("   (♥◠‿◠)ﾉﾞ  wls系统服务-启动完成   ლ(´ڡ`ლ)ﾞ  ");
 	}
 
 }
--- wls-system/src/main/java/cn/erectpine/system/framework/interceptor/SaveLogInterceptor.java
+++ wls-system/src/main/java/cn/erectpine/system/framework/interceptor/SaveLogInterceptor.java
@@ -1,6 +1,10 @@
 package cn.erectpine.system.framework.interceptor;
 
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 保存日志到数据库
@@ -9,5 +13,20 @@ import org.springframework.web.servlet.HandlerInterceptor;
  * @since 2021/3/16 10:31
  */
 public class SaveLogInterceptor implements HandlerInterceptor {
+    
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+    
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+    
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
     
 }
