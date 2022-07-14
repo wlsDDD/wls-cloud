1a1102d008a33bebfc8baea664fa3d2ccefe9c23
wls <1626961806@qq.com>
2022-07-14 15:35:53 +0800

    commit

--- wls-gencode/src/main/java/cn/erectpine/gencode/WlsGencodeApplication.java
+++ wls-gencode/src/main/java/cn/erectpine/gencode/WlsGencodeApplication.java
@@ -20,7 +20,7 @@ public class WlsGencodeApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext app = SpringApplication.run(WlsGencodeApplication.class, args);
         ConfigurableEnvironment env = app.getEnvironment();
-        log.info("\n\t------------------微服务: {}-{} 启动完成--------------------", env.getProperty("spring.application.name"), env.getProperty("spring.profiles.active"));
+        log.info("\n\t--------------微服务: {}-{} 启动完成----------------", env.getProperty("spring.application.name"), env.getProperty("spring.profiles.active"));
     }
     
 }
